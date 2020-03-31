33df42fc72272f4f4cb2ee416da02f56accad8b3
liangchaofei <chaofei_liang@126.com>
2020-03-31 10:54:25 +0800

    feat:增加session配置

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-03-29 23:54:07
+ * @LastEditTime: 2020-03-31 10:31:21
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /koa2-weibo/src/routes/index.js
+ */
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
@@ -11,8 +19,14 @@ router.get('/string', async (ctx, next) => {
 })
 
 router.get('/json', async (ctx, next) => {
+  const session = ctx.session;
+  if(session.viewNum == null){
+    session.viewNum = 0;
+  }
+  session.viewNum++
   ctx.body = {
-    title: 'koa2 json'
+    title: 'koa2 json',
+    num:session.viewNum
   }
 })
 
